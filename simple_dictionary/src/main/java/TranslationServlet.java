60321b02ed66138d72056dfecfe06cf5a00a403c
dinhson98 <Dinhsonmta55@gmail.com>
2023-06-23 14:24:22 +0700

    up

---
+++ simple_dictionary/src/main/java/TranslationServlet.java
@@ -0,0 +1,33 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "TranslationServlet ", value = "/translate")
+
+public class TranslationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin Chao");
+        dictionary.put("how", "The nao");
+        dictionary.put("book", "Quyen vo");
+        dictionary.put("Computer", "May tinh");
+        String search = req.getParameter("txtSearch");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+        writer.println("</html>");
+    }
+}
