9e968c8d48ecfb94bc9501b60deea021686bdac4
dinhson98 <Dinhsonmta55@gmail.com>
2023-06-23 09:14:56 +0700

    up

--- server-time/src/main/java/ServerTimeServlet.java
+++ server-time/src/main/java/ServerTimeServlet.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ServerTimeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", urlPatterns = "/index")
+
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello world</h1>");
+        writer.println("<br>");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+
+    }
 }
